594600cf73b62a79ea67fc3426b57e166a88ef45
DelaRosa_Jose <josealberto3053@gmail.com>
2025-09-30 22:42:05 -0400

    WIP: The AOT Library run in a console App

---
+++ DbRosetta.Native/JsonContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+using DbRosetta.Core; // Asegúrate de que esto apunte al namespace de tu clase MigrationRequest
+
+namespace DbRosetta.Native;
+
+// Esta clase le indica al generador de código fuente qué tipos necesita preparar.
+[JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase, WriteIndented = true)]
+[JsonSerializable(typeof(MigrationRequest))]
+internal partial class AppJsonSerializerContext : JsonSerializerContext
+{
+}
